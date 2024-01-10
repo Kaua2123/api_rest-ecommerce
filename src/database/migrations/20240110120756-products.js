55b400cd96687af1157730744946d25175eee471
Kaua2123 <kauantnetsoares@gmail.com>
2024-01-10 09:38:53 -0300

    model de product finalizado

--- src/database/migrations/20240110120756-products.js
+++ src/database/migrations/20240110120756-products.js
@@ -1,22 +1,38 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      stock_quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  // async down(queryInterface, Sequelize) {},
 };
---
+++ src/models/Product.js
@@ -0,0 +1,50 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class Product extends Model {
+  static init(sequelize) {
+    super.init({
+      name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 24],
+            msg: 'Username must have between 3 or 24 characters',
+          },
+        },
+      },
+      description: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 200],
+            msg: 'Description must have between 3 or 200 characters',
+          },
+        },
+      },
+      price: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0.00,
+        validate: {
+          isFloat: {
+            msg: 'Invalid value',
+          },
+        },
+      },
+      stock_quantity: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+        validate: {
+          isInt: {
+            msg: 'Invalid value',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+
+    return this;
+  }
+}
